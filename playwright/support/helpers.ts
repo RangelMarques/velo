d6a3c83e06a5310a9c60d471a51d93a24db3747a
Rangel Marques <rangeelmarques@gmail.com>
2026-05-13 10:55:48 -0300

    adicionando IA nos estudos, aula relacionada a linguagem natural, cenarios com IA

--- playwright/support/helpers.ts
+++ playwright/support/helpers.ts
@@ -1,21 +1,13 @@
 export function generateOrderCode() {
-    const prefix = 'VLO';
+    const prefix = 'VLO'
 
-    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-    let randomPart = '';
+    const chars = 'ABCGERGHIJHGFNOPREWTUVDFSZ0125456549'
+    let randomPart = ''
 
-    // Gera 3 letras para o prefixo (ex: VLO)
     for (let i = 0; i < 6; i++) {
-        const randomIndex = Math.floor(Math.random() * chars.length);
-        randomPart += chars[randomIndex];
+        const randomIndex = Math.floor(Math.random() * chars.length)
+        randomPart += chars[randomIndex]
     }
 
-    return `${prefix}-${randomPart}`;
-}
-
-import { Page } from '@playwright/test'
-
-export async function searchOrder(page: Page, orderNumber: string) {
-    await page.getByRole('textbox', { name: 'Código do Pedido' }).fill(orderNumber)
-    await page.getByRole('button', { name: 'Buscar Pedido' }).click()
+    return `${prefix}-${randomPart}`
 }
