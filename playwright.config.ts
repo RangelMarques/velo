88fd179361c1ed580ae8cd233005da294db1de2f
Rangel Marques <rangeelmarques@gmail.com>
2026-05-21 16:59:45 -0300

    att base_url para ambiente preview

--- playwright.config.ts
+++ playwright.config.ts
@@ -45,7 +45,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: process.env.BASE_URL || 'https://velo-rangelmarques.vercel.app/',
+    baseURL: process.env.BASE_URL || 'https://velo-bc4vfg014-rangeelmarques-9993s-projects.vercel.app/',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on',
